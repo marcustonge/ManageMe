f6062fcbe191b58c6a185f01c21e8247211add61
marcustonge <marcustonge@hotmail.co.uk>
2021-11-23 19:14:16 +0000

    Added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'active-events', to: "events#active_events"
   get 'contact', to: 'home#contact'
+  post 'request_contact', to: 'home#request_contact'
+  
   root to: 'home#index'
   resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
